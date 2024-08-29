4f1fece160fedd83e3f2f6c47816e1b3f4b28c21
Alik Sargsyan <alik.sargsyan@coderepablic.am>
2024-08-30 01:42:17 +0400

    constant variables of command

---
+++ src/homework1/bookstorage/CommandConstant.java
@@ -0,0 +1,13 @@
+package homework1.bookstorage;
+
+public abstract class CommandConstant {
+  public static final String INPUT = "Please enter";
+  public static final String INVALID_COMMAND = "Invalid command";
+  public static final String ZERO = "0";
+  public static final String ONE = "1";
+  public static final String TWO = "2";
+  public static final String THREE = "3";
+  public static final String FOUR = "4";
+  public static final String FIVE = "5";
+  public static final String SIX = "6";
+}
