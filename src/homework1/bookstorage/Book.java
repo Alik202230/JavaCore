38405b21e54a6c5846b65292a64f88fe7588f52b
Alik Sargsyan <alik.sargsyan@coderepablic.am>
2024-08-30 01:39:31 +0400

    create book entity

---
+++ src/homework1/bookstorage/Book.java
@@ -0,0 +1,87 @@
+package homework1.bookstorage;
+
+import java.util.Objects;
+
+public class Book {
+  private String id;
+  private String title;
+  private String author;
+  private double price;
+  private int quantity;
+
+  public Book(String id, String title, String author, double price, int quantity) {
+    this.id = id;
+    this.title = title;
+    this.author = author;
+    this.price = price;
+    this.quantity = quantity;
+  }
+
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getAuthor() {
+    return author;
+  }
+
+  public void setAuthor(String author) {
+    this.author = author;
+  }
+
+  public double getPrice() {
+    return price;
+  }
+
+  public void setPrice(double price) {
+    this.price = price;
+  }
+
+  public int getQuantity() {
+    return quantity;
+  }
+
+  public void setQuantity(int quantity) {
+    this.quantity = quantity;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    Book book = (Book) o;
+    return Double.compare(price, book.price) == 0
+        && Objects.equals(id, book.id)
+        && Objects.equals(title, book.title)
+        && Objects.equals(author, book.author)
+        && Objects.equals(quantity, book.quantity);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id, title, author, price, quantity);
+  }
+
+  @Override
+  public String toString() {
+    return "Book{" +
+        "id='" + id + '\'' +
+        ", title='" + title + '\'' +
+        ", author='" + author + '\'' +
+        ", price=" + price + '\'' +
+        ", quantity=" + quantity +
+        '}';
+  }
+}
