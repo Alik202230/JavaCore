81ea0cfaab41a7dfae9b221ce4a23853fb9797f8
Alik Sargsyan <alik.sargsyan@coderepablic.am>
2024-08-30 01:41:07 +0400

    constant variables of book

---
+++ src/homework1/bookstorage/BookConstant.java
@@ -0,0 +1,11 @@
+package homework1.bookstorage;
+
+public abstract class BookConstant {
+  public static final String EXIT = "EXIT";
+  public static final String ADD_BOOK = "ADD BOOK";
+  public static final String PRINT_ALL_BOOK = "PRINT ALL BOOK";
+  public static final String SEARCH_BY_BOOK_TITLE = "SEARCH BY TITLE";
+  public static final String UPDATE_BOOK = "UPDATE BOOK";
+  public static final String DELETE_BOOK = "DELETE BOOK";
+  public static final String BOOK_BY_RANGE = "BOOK BY RANGE";
+}
