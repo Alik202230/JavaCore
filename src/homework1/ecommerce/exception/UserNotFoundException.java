2c8ef5d085fe59bc1aeb75c835314769e4e87c86
Alik Sargsyan <alik.sargsyan@coderepablic.am>
2024-09-20 03:40:24 +0400

    Users exception

---
+++ src/homework1/ecommerce/exception/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package homework1.ecommerce.exception;
+
+public class UserNotFoundException extends RuntimeException {
+  public UserNotFoundException(String message) {
+    super(message);
+  }
+}
