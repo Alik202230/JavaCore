f725845c4f77d40c608a5c514e67f6d18a370ee5
Alik Sargsyan <alik.sargsyan@coderepablic.am>
2024-09-20 03:40:10 +0400

    Products exception

---
+++ src/homework1/ecommerce/exception/ProductAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package homework1.ecommerce.exception;
+
+public class ProductAlreadyExistsException extends RuntimeException {
+  public ProductAlreadyExistsException(String message) {
+    super(message);
+  }
+}
